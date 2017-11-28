46ddc90f53232439adb61ea6abf0573fd295d9fb
Yule <recombinationer@gmail.com>
2017-11-28 14:39:47 +0800

    add card 066

---
+++ cards/Sim_AT_066.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREngine.Bots
+{
+	class Sim_AT_066 : SimTemplate //* Orgrimmar Aspirant
+	{
+		//Inspire: Give your weapon +1 Attack.
+
+		public override void onInspire(Playfield p, Minion m, bool own)
+        {
+			if (m.own == own)
+			{
+                if (own)
+                {
+                    if (p.ownWeaponDurability > 0) p.ownWeaponAttack++;
+                }
+                else
+                {
+                    if (p.enemyWeaponDurability > 0) p.enemyWeaponAttack++;
+                }
+			}
+        }
+	}
+}
